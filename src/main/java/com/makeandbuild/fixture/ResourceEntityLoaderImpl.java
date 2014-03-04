2aa084dfafa79274c3b7898d315625d196809a2a
bahzuercher <az@ehirelabs.com>
2014-03-04 14:20:57 -0500

    fix for boolean mapped datatypes

--- src/main/java/com/makeandbuild/fixture/ResourceEntityLoaderImpl.java
+++ src/main/java/com/makeandbuild/fixture/ResourceEntityLoaderImpl.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.type.JavaType;
@@ -14,6 +17,8 @@ public class ResourceEntityLoaderImpl implements EntityLoader {
     @SuppressWarnings("rawtypes")
     private Class entityClass;
     private String resourcePath;
+    private static Log logger = LogFactory.getLog(ResourceEntityLoaderImpl.class);
+    
     public ResourceEntityLoaderImpl(String resourcePath) throws ClassNotFoundException{
         super();
         String classname = resourcePath.replace(".json", "");
@@ -34,16 +39,22 @@ public class ResourceEntityLoaderImpl implements EntityLoader {
         if (mapper == null) {
             mapper = new ObjectMapper();
             mapper.getSerializationConfig().setSerializationInclusion(Inclusion.NON_NULL);
+            mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
         }
         return mapper;
     }
     @Override
     public List<Object> load() throws IOException {
-        String json = IOUtils.toString(this.getClass().getResourceAsStream(resourcePath));
-        ArrayNode node = (ArrayNode) getInstance().readTree(json);
-        JavaType type = mapper.getTypeFactory().
-                constructCollectionType(List.class, entityClass);
-        return getInstance().readValue(node, type);
+        try {
+            String json = IOUtils.toString(this.getClass().getResourceAsStream(resourcePath));
+            ArrayNode node = (ArrayNode) getInstance().readTree(json);
+            JavaType type = mapper.getTypeFactory().
+                    constructCollectionType(List.class, entityClass);
+            return getInstance().readValue(node, type);
+        }catch (IOException e){
+            logger.error("problem loading resource "+resourcePath, e);
+            throw e;
+        }
 
     }
 }
